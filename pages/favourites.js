8029dda3396c5138dcdbbcd2b69181a3939a2f22
Abdullah <abdullah1@myseneca.ca>
2023-08-05 21:24:10 -0400

    link change

--- pages/favourites.js
+++ pages/favourites.js
@@ -1,42 +1,42 @@
+import Error from 'next/error';
 import { Row, Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import ArtworkCard from '../components/ArtworkCard';
+import ArtworkCard from '../components/ArtworkCard'
 import { useAtom } from 'jotai';
 import { favouritesAtom } from '../store';
-import { useState } from 'react';
-
 
 export default function Favourites() {
-  const [favouritesList] = useAtom(favouritesAtom);
 
-  if (!favouritesList) return null;
+  const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
 
-  return (
-    <>
-      <Row className="gy-4">
-        {favouritesList.length > 0 ? (
-          favouritesList.map((item) => {
-            return (
-              <Col lg={3} key={item}>
-                <ArtworkCard objectID={item} />
-              </Col>
-            );
-          })
-        ) : (
-          <Card>
-            <Card.Body>
-              <Card.Text>
-                <strong>Nothing Here</strong>
-                <br />
-                Try adding some new artwork to the list.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        )}
-      </Row>
-      {
+  if(!favouritesList) return null;
+  
+  if(favouritesList){
+    return (
+      <>
+        <Row className="gy-4">
+          {favouritesList.length > 0 ?
+            favouritesList.map(item => {
+              return (
+                <Col lg={3} key={item}><ArtworkCard objectID={item} /></Col>
+              )
+            })
+            :
+            <Card>
+              <Card.Body>
+                <Card.Text>
+                  <strong>Nothing Here</strong><br />
+                  Try adding for something else.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          }
+        </Row>
+        {
           !favouritesList && null 
-      }
-    </>
-  );
+        }
+      </>
+    )
+  }
+  
 }
